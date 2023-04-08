d7e17ac6d8ca17034cbd98ff7a358a324fd68ffc
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-04-08 23:14:41 +0200

    Update ConsoleColors.java

--- Code/ConsoleColors.java
+++ Code/ConsoleColors.java
@@ -1,6 +1,6 @@
 public class ConsoleColors {
     // RESET
-    public static final String RESET = "\033[0m";  // Text Reset
+    public static final String RESET = "\033[0m";  // TEXT RESET
 
     // REGULAR COLORS
     public static final String BLACK = "\033[0;30m";   // BLACK
@@ -63,12 +63,12 @@ public class ConsoleColors {
     public static final String WHITE_BOLD_BRIGHT = "\033[1;97m"; // WHITE
 
     // HIGH INTENSITY BACKGROUNDS
-    public static final String BLACK_BACKGROUND_BRIGHT = "\033[0;100m";// BLACK
-    public static final String RED_BACKGROUND_BRIGHT = "\033[0;101m";// RED
-    public static final String GREEN_BACKGROUND_BRIGHT = "\033[0;102m";// GREEN
+    public static final String BLACK_BACKGROUND_BRIGHT = "\033[0;100m"; // BLACK
+    public static final String RED_BACKGROUND_BRIGHT = "\033[0;101m";   // RED
+    public static final String GREEN_BACKGROUND_BRIGHT = "\033[0;102m"; // GREEN
     public static final String YELLOW_BACKGROUND_BRIGHT = "\033[0;103m";// YELLOW
-    public static final String BLUE_BACKGROUND_BRIGHT = "\033[0;104m";// BLUE
-    public static final String PURPLE_BACKGROUND_BRIGHT = "\033[0;105m"; // PURPLE
+    public static final String BLUE_BACKGROUND_BRIGHT = "\033[0;104m";  // BLUE
+    public static final String PURPLE_BACKGROUND_BRIGHT = "\033[0;105m";// PURPLE
     public static final String CYAN_BACKGROUND_BRIGHT = "\033[0;106m";  // CYAN
-    public static final String WHITE_BACKGROUND_BRIGHT = "\033[0;107m";   // WHITE
+    public static final String WHITE_BACKGROUND_BRIGHT = "\033[0;107m"; // WHITE
 }
